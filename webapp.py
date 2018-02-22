508e08a3a87cdaa2cc59c96b76adda37be09baad
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-02-22 08:36:18 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -57,10 +57,11 @@ def post():
     return render_template('home.html', past_posts=posts_to_html(), rar=mess)
 
 def posts_to_html():
+    post = "something"
     try:
         with open(file,'r+') as jsonFile:
             data = json.load(jsonFile)
-    except:
+    except Exception as e:
         post = "nothing"
     return post
 
