06a2bcc226d7da6c6eeb458cc0e256d3a11365ff
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-02-22 09:09:48 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -63,8 +63,8 @@ def posts_to_html():
     try:
         with open(file,'r+') as jsonFile:
             data = json.load(jsonFile)
-            for key in data:
-                post = Markup("<p>" + key + " " + data[0][key] + "</p>")
+            for items in data:
+                post = Markup("<p>" + items + "</p>")
     except Exception as e:
         print(e)
         post = Markup("<p>Nothing</p>")
