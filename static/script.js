7364bbb0228e064a26c0a862aa94bdef93b2a062
Luke Borders <31492762+lukeborders@users.noreply.github.com>
2018-02-23 09:30:16 -0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,14 @@
+document.ready(function(){
+  function underLineChar() {
+    var specialChar = '@';
+    if $("#textbox").val() == specialChar {
+        $("#textbox").val().css('text-decoration','underline');
+        else {
+          $("#textbox").val().css('text-decoration','none');
+        }
+    
+    }
+  }
+
+});
 
