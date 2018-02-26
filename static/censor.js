8cdf72d554ee56979645887fa0dc62f0b4a72f91
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-02-26 09:29:51 -0800

    Update censor.js

--- static/censor.js
+++ static/censor.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-  
-})
+  var profanity = ["f-word","lorax","c-word"];
+  for(int i = 0; i < 3; i++){
+    if(profanity[i] == $("#postedmessage").val()){
+        $("#postedmessage").val() = "Not a friendly enough message, go back to school.";
+    }
+  }
+});
