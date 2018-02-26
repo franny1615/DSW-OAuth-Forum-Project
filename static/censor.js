84b20ca223eea4c47a133d2337d2f850e717b192
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-02-26 09:44:16 -0800

    Update censor.js

--- static/censor.js
+++ static/censor.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-	var profanity = ["f-word","lorax","c-word"]; 
-  for(var i = 0; i < 3; i++){
-    if(profanity[i] == $("#postedmessage").val()){
-        $("#postedmessage").val() = "Not a friendly enough message, go back to school.";
+    var profanity = ["f-word","lorax","c-word"]; 
+    for(var i = 0; i < 3; i++){
+        if(profanity[i] == $("#postedmessage").val()){
+            $("#postedmessage").val() = "Not a friendly enough message, go back to school.";
+        }
     }
-  }
 });
