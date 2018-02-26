c691673e5879dd552778da0ffaec3ddfd682cae1
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-02-26 09:59:01 -0800

    Delete censor.js

--- static/censor.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    var profanity = ["f-word","lorax","c-word"];
-    var post = $('#postedmessage').val();
-    for(var i = 0; i < 3; i++){
-       if(post == profanity[i]){
-           $("#postedmessage").val() = "Not a friendly enough message, go back to school.";
-       }
-    }
-});
