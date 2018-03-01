1da342e087e6da5326679905cb976cd3294d9bd6
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-03-01 10:14:42 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -60,7 +60,7 @@ def posts_to_html():
         with open(file,'r+') as jsonFile:
             data = json.load(jsonFile)
             for stuff in data:
-                post += "<table id='postTable'><tr><td class='un'><b>Username</b></td><td class='post'><b>Post</b></td></tr>" + '<tr>' + '<td class="un"><img src=session['user_data']['avatar_url']  height="50" width="50"><a href=' + '"https://github.com/' + stuff[0] + '">'+ '@' + stuff[0] +'</a>' + '</td><td class="post">'
+                post += "<table id='postTable'><tr><td class='un'><b>Username</b></td><td class='post'><b>Post</b></td></tr>" + '<tr>' + '<td class="un"><img src="session['user_data']['avatar_url']"  height="50" width="50"><a href=' + '"https://github.com/' + stuff[0] + '">'+ '@' + stuff[0] +'</a>' + '</td><td class="post">'
                 swearwords = ['lorax','fuck','c-word','n-word','heckin']
                 if '@' in stuff[1]:
                     username = ""
