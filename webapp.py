2388dbe42ba59d0b4e44a874d770175ae244b942
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-03-13 09:33:03 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,12 +5,26 @@ from flask import render_template
 import pprint
 import os
 import json
+import pymongo
+import sys
 
 app = Flask(__name__)
 
 app.debug = True #Change this to False for production
 
 app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
+
+url = 'mongodb://{}:{}@{}:{}/{}'.format(
+        os.environ["MONGO_USERNAME"],
+        os.environ["MONGO_PASSWORD"],
+        os.environ["MONGO_HOST"],
+        os.environ["MONGO_PORT"],
+        os.environ["MONGO_DBNAME"])
+    
+client = pymongo.MongoClient(url)
+db = client[os.environ["MONGO_DBNAME"]]
+collection = db['forum-posts'] #put the name of your collection in the quotes
+
 oauth = OAuth(app)
 
 #Set up GitHub as OAuth provider
