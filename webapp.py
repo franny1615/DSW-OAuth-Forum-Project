e2ffc81d542c54793b49319b4dcadde9936f255b
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-03-13 09:38:42 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -51,7 +51,7 @@ def inject_logged_in():
 
 @app.route('/')
 def home():
-    return render_template('home.html', past_posts=posts_to_html(), testMDB=collection.find_one())
+    return render_template('home.html', past_posts=posts_to_html())
 
 @app.route('/posted', methods=['POST'])
 def post():
