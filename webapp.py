b4d933d235fdfecc79daaebbe8845a07ea6610bc
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-03-16 09:30:28 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -95,6 +95,8 @@ def posts_to_html():
 @app.route('/deletePost', methods=['POST']) #this does things
 def deletePost():
     #delete post
+    #collection.deleteOne({“_id” : ObjectId(“”)})
+    print(request.form)
     return render_template('home.html', past_posts=posts_to_html())
 
 #redirect to GitHub's OAuth page and confirm callback URL
