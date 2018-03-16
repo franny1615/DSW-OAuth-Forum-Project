8c514f13dc9d64750b0a27bad72117769d2fcfdb
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-03-16 09:26:06 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -86,7 +86,7 @@ def posts_to_html():
                 post += "Offensive language is not tolerated."
             else:
                 post += document['post'][1]
-            post += '</td><td><form action="/deletePost" method="post"><input type="submit" name="'+ document.get('_id') +'" value="Delete" class="btn btn-danger"></form></td></tr></table>'
+            post += '</td><td><form action="/deletePost" method="post"><input type="submit" name="'+ str(document.get('_id')) +'" value="Delete" class="btn btn-danger"></form></td></tr></table>'
     except Exception as e:
         print(e)
     formattedPost = Markup(post)
